d2270b0afc042ec2eb697861063592f71bec064f
Tune <atune313@gmail.com>
2019-12-03 18:13:03 +0000

    aa

---
+++ src/days/three.rs
@@ -0,0 +1,10 @@
+use std::collections::HashMap;
+use crate::utils;
+
+fn one() -> utils::Result<i32> {
+    let input: Vec<String> = utils::get_lines(utils::path("three.txt"))?;
+    let map: HashMap<(i32, i32), bool> = HashMap::new();
+    
+
+    Ok(0)
+}
